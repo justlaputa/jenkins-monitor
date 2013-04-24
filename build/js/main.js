7fc85baafcd65656efe65cec430f94fa677cdb44
justlaputa <justlaputa@gmail.com>
2013-04-24 23:21:37 +0800

    test with correct jenkins job data

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,48 @@
-$(function() {
-    var jenkinsUrl = 'jenkins.json';
+/** json job status color:
+ * "aborted"
+ * "aborted_anime"
+ * "blue"
+ * "blue_anime"
+ * "disabled"
+ * "grey"
+ * "red"
+ * "red_anime"
+ * "yellow"
+ * "yellow_anime"
+ */
 
-    if(chrome && chrome.extension) {
-        jenkinsUrl = chrome.extension.getURL('jenkins.json');
+$(function() {
+    var remoteUrl = 'https://builds.apache.org/api/json?tree=jobs[color,name]',
+        localUrl = 'jenkins.json',
+
+        color_map = {
+            'aborted': '', //aborted
+            'aborted_anime': '', //aborted
+            'blue': 'success',//success
+            'blue_anime': 'success',//success
+            'disabled': 'inverse',//disabled
+            'disabled_anime': 'inverse',//disabled
+            'grey': 'info',    //pending
+            'grey_anime': 'info',    //pending
+            'red': 'important',     //failed
+            'red_anime': 'important',     //failed
+            'yellow': 'warning',   //unstable
+            'yellow_anime': 'warning'   //unstable
+        };
+
+    function processData(data) {
+        data['jobs'].forEach(function(job) {
+            var bootstrap_type = color_map[job.color];
+
+            if (bootstrap_type) {
+                job.color_type = bootstrap_type;
+            }
+
+            if (job.color.search(/_anime$/) !== -1) {
+                job.in_progress = true;
+            }
+        });
+
     }
     
     function showLoadingSpin(show) {
@@ -21,9 +61,10 @@ $(function() {
     showLoadingSpin(true);
 
     $.ajax({
-        url: jenkinsUrl,
+        url: localUrl,
         dataType: 'json',
         success: function(data) {
+            processData(data);
 
             showLoadingSpin(false);
 

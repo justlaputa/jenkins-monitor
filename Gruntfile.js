e50fe09236019f270a53e94765f6b59b060232bb
justlaputa <justlaputa@gmail.com>
2013-10-06 12:40:37 +0900

    add copy library files task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,49 @@ module.exports = function(grunt) {
             }
         },
 
+        copy: {
+            lib: {
+                files: [
+                {
+                    expand: true,
+                    flatten: true,
+                    dest: 'extension/js/lib/',
+                    src: [
+                    'bower_components/jquery/jquery.js',
+                    'bower_components/jquery/jquery.min.map',
+                    'bower_components/bootstrap/docs/assets/js/bootstrap.js',
+                    'bower_components/handlebars/handlebars.runtime.js'
+                    ]
+                },
+                {
+                    expand: true,
+                    flatten: true,
+                    dest: 'extension/css/',
+                    src: [
+                    'bower_components/bootstrap/docs/assets/css/bootstrap*.css',
+                    'bower_components/font-awesome/css/font-awesome.css'
+                    ]
+                },
+                {
+                    expand: true,
+                    flatten: true,
+                    dest: 'extension/img/',
+                    src: [
+                    'bower_components/bootstrap/docs/assets/img/glyphicons*.png',
+                    ]
+                },
+                {
+                    expand: true,
+                    flatten: true,
+                    dest: 'extension/font/',
+                    src: [
+                    'bower_components/font-awesome/font/*'
+                    ]
+                }
+                ]
+            }
+        },
+
         handlebars: {
             compile: {
                 options: {
@@ -29,7 +72,8 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-handlebars');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('build', ['handlebars']);
+    grunt.registerTask('build', ['handlebars', 'copy:lib']);
 
 };

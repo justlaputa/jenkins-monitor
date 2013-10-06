fa142be70f3c3dd4bbf5480cf8b3da6f435e9b6b
justlaputa <justlaputa@gmail.com>
2013-10-06 13:51:46 +0900

    add option for refresh time in settings page

--- extension/js/options.js
+++ extension/js/options.js
@@ -1,35 +1,58 @@
 (function(window, $) {
 
-    function save_options() {
-        var input = $('#jenkins-url'),
-            info = input.next(),
-            url = input.val();
+    function save_options(option, callback) {
 
-        if (url === '') {
-            url = null;
-        }
+        chrome.storage.local.get('options', function(items) {
+            var options = items['options'] || {},
+            name;
 
-        chrome.storage.local.set({'jenkins_url': url}, function() {
-            info.addClass('text-success')
-                .text(' saved').prepend($('<i></i>', { 'class': 'icon-ok' }));
-        });
+            for (name in option) {
+                options[name] = option[name];
+            }
 
-        chrome.runtime.getBackgroundPage(function(backend) {
-            backend.restart();
+            chrome.storage.local.set({'options': options}, function() {
+                callback();
+
+                chrome.runtime.getBackgroundPage(function(backend) {
+                    backend.restart();
+                });
+            });
         });
     }
-
 
     function restore_options() {
-        var input = $('#jenkins-url');
 
-        chrome.storage.local.get('jenkins_url', function(items) {
-            input.val(items['jenkins_url']);
+        chrome.storage.local.get('options', function(items) {
+            var options = items['options'],
+            name,
+            val;
+
+            for (name in options) {
+                val = options[name];
+
+                $('#options-form input[name="' + name + '"]').val(val);
+            }
         });
     }
 
-    $(document).on('change', '#jenkins-url', function() {
-        save_options();
+    $(document).on('change', '#options-form input', function() {
+        var input = $(this),
+        option = {},
+        name = input.attr('name'),
+        value = input.val();
+
+        option[name] = value;
+
+        save_options(option, function() {
+            if (input.attr('type') === 'range') {
+                input.next().val(value);
+            }
+
+            input.parent().find('span.save-info')
+            .addClass('text-success')
+            .text(' saved')
+            .prepend($('<i></i>', { 'class': 'icon-ok' }));
+        });
     });
 
     $(function() {
